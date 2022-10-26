5f1e05b30aa16a1a33c9c3dfdb6f2aecfcc6bd1f
btanti <bbt27@cornell.edu>
2022-10-25 22:20:39 -0700

    polybar

--- i3/scripts/monitor.sh
+++ i3/scripts/monitor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-xrandr --output DP-2 --primary
-xrandr --output HDMI-0 --left-of DP-2
-xrandr --output DP-0 --right-of DP-2
+xrandr --output DP-0 --primary
+xrandr --output HDMI-0 --left-of DP-0
+xrandr --output DP-4 --right-of DP-0
